b8681b7a1d4962761934103a091d49a70d7126bd
Prisha Gupta <prishapg101@gmail.com>
2023-08-23 08:18:55 +0530

    Added singup page

--- src/components/common/Chats.jsx
+++ src/components/common/Chats.jsx
@@ -1,114 +1,120 @@
-import "./Chats.scss";
+import "./Chats.scss"
 
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { socket } from "../../lib/constants";
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { socket } from "../../lib/constants"
 
 /// a, b, c -> client emit (socket.emit), serven listen (socket.on)
 /// d, e, f -> server emit (socket.emit), client listen (socket.on)
 
 export default function Chats(props) {
-  //chat navigation
-  const navigate = useNavigate();
-  const [chats, setChats] = useState([]);
+	//chat navigation
+	const navigate = useNavigate()
+	const [chats, setChats] = useState([])
 
-  //user info is need here only and userid is already global
-  const [user, setUser] = useState(null);
+	//user info is need here only and userid is already global
+	const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    //this will chnage on change of userid first->off then on to avoide duplicate listners
-    socket.off("user");
-    socket.emit("sendUser", { token: localStorage.getItem("token") });
-    socket.on("user", (data) => {
-      if (data.status !== 200) {
-        localStorage.clear();
-        props.setUserID(null);
-        return;
-      }
-      const user = data.user;
-      setUser(user);
-    });
+	useEffect(() => {
+		//this will chnage on change of userid first->off then on to avoide duplicate listners
+		socket.off("user")
+		socket.emit("sendUser", { token: localStorage.getItem("token") })
+		socket.on("user", (data) => {
+			if (data.status !== 200) {
+				localStorage.clear()
+				props.setUserID(null)
+				return
+			}
+			const user = data.user
+			setUser(user)
+		})
 
-    //callback is provided for the time of unmounting of function
-    return () => {
-      socket.off("user");
-    };
-  }, [props.userID]);
+		//callback is provided for the time of unmounting of function
+		return () => {
+			socket.off("user")
+		}
+	}, [props.userID])
 
-  useEffect(() => {
-    socket.on("chatCreated", () => {
-      socket.emit("sendChats", { userID: props.userID });
-    });
+	useEffect(() => {
+		socket.on("chatCreated", () => {
+			socket.emit("sendChats", { userID: props.userID })
+		})
 
-    return () => {
-      socket.off("chatCreated");
-    };
-  }, []);
+		return () => {
+			socket.off("chatCreated")
+		}
+	}, [])
 
-  useEffect(() => {
-    socket.emit("sendChats", { userID: props.userID });
-    socket.on("chats", (data) => {
-      setChats(data);
-    });
-    //for unmount use callback
-    return () => {
-      socket.off("chats");
-    };
-  }, []);
+	useEffect(() => {
+		socket.emit("sendChats", { userID: props.userID })
+		socket.on("chats", (data) => {
+			setChats(data)
+		})
+		//for unmount use callback
+		return () => {
+			socket.off("chats")
+		}
+	}, [])
 
-  if (!user) return null;
+	if (!user) return null
 
-  return (
-    <div className="ChatsComponent">
-      <div className="header">
-        <div className="dp">
-          <img src={user.dp} alt="" />
-        </div>
-        <div className="search">
-          <input type="text" placeholder="Search" />
-          <div
-            className="action"
-            onClick={() => {
-              const username = prompt("enter username", "someone")?.trim();
-              if (!username) return;
+	return (
+		<div className="ChatsComponent">
+			<div className="header">
+				<div
+					className="dp"
+					onClick={() => {
+						props.setUserID(null)
+						localStorage.clear()
+					}}
+				>
+					<img src={user.dp} alt="" />
+				</div>
+				<div className="search">
+					<input type="text" placeholder="Search" />
+					<div
+						className="action"
+						onClick={() => {
+							const username = prompt("enter username", "someone")?.trim()
+							if (!username) return
 
-              socket.emit("newChat", {
-                userID: props.userID,
-                username: username,
-              });
-            }}
-          >
-            <i className="fi fi-rr-edit"></i>
-          </div>
-        </div>
-      </div>
-      <div className="chats">
-        {!chats.length ? (
-          <p>connect to people to start chatting</p>
-        ) : (
-          chats.map((chat) => {
-            return (
-              <div
-                className={`chatCard ${props.id === chat.id ? "active" : ""}`}
-                key={chat.id}
-                onClick={() => {
-                  navigate("/" + chat.id);
-                }}
-              >
-                <div className="profile">
-                  <img src={chat.participants[0].dp} alt="dp" />
-                </div>
-                <div className="box">
-                  <div className="name">{chat.participants[0].name}</div>
-                  <div className="username">
-                    {chat.participants[0].username}
-                  </div>
-                </div>
-              </div>
-            );
-          })
-        )}
-      </div>
-    </div>
-  );
+							socket.emit("newChat", {
+								userID: props.userID,
+								username: username,
+							})
+						}}
+					>
+						<i className="fi fi-rr-edit"></i>
+					</div>
+				</div>
+			</div>
+			<div className="chats">
+				{!chats.length ? (
+					<p>connect to people to start chatting</p>
+				) : (
+					chats.map((chat) => {
+						return (
+							<div
+								className={`chatCard ${props.id === chat.id ? "active" : ""}`}
+								key={chat.id}
+								onClick={() => {
+									navigate("/" + chat.id)
+								}}
+							>
+								<div className="profile">
+									<img src={chat.participants[0].dp} alt="dp" />
+								</div>
+								<div className="box">
+									<div className="name">{chat.participants[0].name}</div>
+									<div className="username">
+										{chat.participants[0].username}
+									</div>
+								</div>
+							</div>
+						)
+					})
+				)}
+			</div>
+		</div>
+	)
 }
--- src/components/routes/Login.jsx
+++ src/components/routes/Login.jsx
@@ -1,100 +1,192 @@
-import "./Login.scss";
+import "./Login.scss"
 
-import { useState } from "react";
-import logo from "../../assets/logo.svg";
+import { useState } from "react"
+import logo from "../../assets/logo.svg"
 
 export default function Login(props) {
-  const [username, setUsername] = useState("username");
-  const [password, setPassword] = useState("Hello@123");
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+	const [name, setName] = useState("My cat")
+	const [email, setEmail] = useState("cat@vibeline.com")
+	const [username, setUsername] = useState("my_cat")
+	const [password, setPassword] = useState("Hello@123")
+	const [phone, setPhone] = useState("123456890")
 
-  return (
-    <div className="LoginComponent Route">
-      <div className="wrapper">
-        <div className="logo">
-          <img src={logo} alt="Logo" />
-        </div>
-        <div className="main">
-          <div className="input">
-            <i className="fi fi-rr-at"></i>
-            <input
-              autoFocus
-              type="text"
-              value={username}
-              onKeyDown={(e) => {
-                if (e.key === "Enter")
-                  document.getElementById("login-button")?.click();
-              }}
-              placeholder="Username"
-              autoComplete="bingsights-email"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="divider"></div>
-          <div className="input">
-            <i className="fi fi-rr-key"></i>
-            <input
-              type="password"
-              value={password}
-              onKeyDown={(e) => {
-                if (e.key === "Enter")
-                  document.getElementById("login-button")?.click();
-              }}
-              placeholder="Password"
-              autoComplete="vibeline-password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="sub">
-            <div className="error">{error}</div>
-            <div className="forgot">SignUp</div>
-          </div>
-          <div
-            id="login-button"
-            className="button"
-            onClick={() => {
-              fetch("http://localhost:4000/auth/login", {
-                method: "POST",
-                headers: {
-                  "content-type": "application/json",
-                },
-                body: JSON.stringify({
-                  username: username,
-                  password: password,
-                }),
-              }).then(async (res) => {
-                const body = await res.json();
-                if (res.status !== 200) {
-                  setError(body.message);
-                } else {
-                  setError("");
-                  props.setUserID(body.user._id);
+	const [isLogin, setIsLogin] = useState(true)
 
-                  localStorage.setItem("token", body.token);
-                  localStorage.setItem("userID", body.user._id);
-                }
-              });
-            }}
-          ></div>
-        </div>
-        <div className="divider">
-          <div className="line"></div>
-          <span>or</span>
-          <div className="line"></div>
-        </div>
-        <div className="alternatives">
-          <div
-            className="alternative"
-            onClick={() => {
-              console.log("Logging with gOOgle");
-            }}
-          >
-            {/* <img src={GoogleLogo} alt="Logo" /> */}
-            <span>Continue with Google</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const [error, setError] = useState("")
+
+	return (
+		<div className="LoginComponent Route">
+			<div className="wrapper">
+				<div className="logo">
+					<img src={logo} alt="Logo" />
+				</div>
+				<div className="main">
+					{!isLogin && (
+						<>
+							<div className="input">
+								<i className="fi fi-rr-text"></i>
+								<input
+									autoFocus
+									type="text"
+									value={name}
+									onKeyDown={(e) => {
+										if (e.key === "Enter")
+											document.getElementById("login-button")?.click()
+									}}
+									placeholder="Name"
+									autoComplete="vibeline-email"
+									onChange={(e) => setName(e.target.value)}
+								/>
+							</div>
+							<div className="divider"></div>
+							<div className="input">
+								<i className="fi fi-rr-envelope"></i>
+								<input
+									autoFocus
+									type="text"
+									value={email}
+									onKeyDown={(e) => {
+										if (e.key === "Enter")
+											document.getElementById("login-button")?.click()
+									}}
+									placeholder="Email"
+									autoComplete="vibeline-email"
+									onChange={(e) => setEmail(e.target.value)}
+								/>
+							</div>
+							<div className="divider"></div>
+							<div className="input">
+								<i className="fi fi-rr-phone-call"></i>
+								<input
+									autoFocus
+									type="text"
+									value={phone}
+									onKeyDown={(e) => {
+										if (e.key === "Enter")
+											document.getElementById("login-button")?.click()
+									}}
+									placeholder="Phone"
+									autoComplete="vibeline-phone"
+									onChange={(e) => setPhone(e.target.value)}
+								/>
+							</div>
+							<div className="divider"></div>
+						</>
+					)}
+					<div className="input">
+						<i className="fi fi-rr-at"></i>
+						<input
+							autoFocus
+							type="text"
+							value={username}
+							onKeyDown={(e) => {
+								if (e.key === "Enter")
+									document.getElementById("login-button")?.click()
+							}}
+							placeholder="Username"
+							autoComplete="vibeline-email"
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+					</div>
+					<div className="divider"></div>
+					<div className="input">
+						<i className="fi fi-rr-key"></i>
+						<input
+							type="password"
+							value={password}
+							onKeyDown={(e) => {
+								if (e.key === "Enter")
+									document.getElementById("login-button")?.click()
+							}}
+							placeholder="Password"
+							autoComplete="vibeline-password"
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</div>
+					<div className="sub">
+						<div className="error">{error}</div>
+						<div
+							className="forgot"
+							onClick={() => {
+								setIsLogin(!isLogin)
+							}}
+						>
+							{isLogin ? "Signup" : "Login"}
+						</div>
+					</div>
+					<div
+						id="login-button"
+						className="button"
+						onClick={() => {
+							if (isLogin) {
+								fetch("http://localhost:4000/auth/login", {
+									method: "POST",
+									headers: {
+										"content-type": "application/json",
+									},
+									body: JSON.stringify({
+										username: username,
+										password: password,
+									}),
+								}).then(async (res) => {
+									const body = await res.json()
+									if (res.status !== 200) {
+										setError(body.message)
+									} else {
+										setError("")
+										props.setUserID(body.user._id)
+
+										localStorage.setItem("token", body.token)
+										localStorage.setItem("userID", body.user._id)
+									}
+								})
+							} else {
+								fetch("http://localhost:4000/auth/signup", {
+									method: "POST",
+									headers: {
+										"content-type": "application/json",
+									},
+									body: JSON.stringify({
+										name: name,
+										email: email,
+										username: username,
+										password: password,
+									}),
+								}).then(async (res) => {
+									const body = await res.json()
+									if (res.status !== 200) {
+										setError(body.message)
+									} else {
+										setError("")
+										props.setUserID(body.user._id)
+
+										localStorage.setItem("token", body.token)
+										localStorage.setItem("userID", body.user._id)
+									}
+								})
+							}
+						}}
+					>
+						{isLogin ? "Login" : "Signup"}
+					</div>
+				</div>
+				<div className="divider">
+					<div className="line"></div>
+					<span>or</span>
+					<div className="line"></div>
+				</div>
+				<div className="alternatives">
+					<div
+						className="alternative"
+						onClick={() => {
+							console.log("Logging with gOOgle")
+						}}
+					>
+						{/* <img src={GoogleLogo} alt="Logo" /> */}
+						<span>Continue with Google</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
